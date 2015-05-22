ed4332e9d7ac4ff581fc4ae80f2f8796b633af52
MacMini <macmini@bigdog.local>
2015-05-22 10:38:09 +0800

    Initial Commit

---
+++ DZBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DZBus
+//
+//  Created by MacMini on 15/5/22.
+//  Copyright (c) 2015年 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
