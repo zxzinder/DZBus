072d7160af04f451d6a396044eafc84a94697e32
zxzinder <macmini@bigdog.local>
2015-08-11 17:48:11 +0800

    DZBUS

---
+++ DZBus/主页面/Views/DZBusAdView.h
@@ -0,0 +1,13 @@
+//
+//  DZBusAdView.h
+//  DZBus
+//
+//  Created by MacMini on 15/5/26.
+//  Copyright (c) 2015年 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZBusAdView : UIView
+
+@end
---
+++ DZBus/主页面/Views/DZBusAdView.m
@@ -0,0 +1,21 @@
+//
+//  DZBusAdView.m
+//  DZBus
+//
+//  Created by MacMini on 15/5/26.
+//  Copyright (c) 2015年 MacMini. All rights reserved.
+//
+
+#import "DZBusAdView.h"
+
+@implementation DZBusAdView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
